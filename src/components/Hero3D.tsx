53f22b4511cc79e635fead9f1c128f6b54327204
Tejas Gupta <68655741+CodeByTejas@users.noreply.github.com>
2024-11-18 12:58:59 +0530

    Update Hero3D.tsx

--- src/components/Hero3D.tsx
+++ src/components/Hero3D.tsx
@@ -26,7 +26,8 @@ function AnimatedSphere() {
 
 export default function Hero3D() {
   return (
-    <div className="absolute right-0 top-0 h-[600px] w-1/2">
+     // <div className="absolute right-0 top-0 h-[600px] w-1/2"> 
+      <div className="absolute top-0 right-0 w-full h-[300px] sm:h-[400px] md:h-[500px] lg:h-[600px] xl:h-[700px]">
       <Canvas>
         <ambientLight intensity={1} />
         <directionalLight position={[2, 1, 1]} />
